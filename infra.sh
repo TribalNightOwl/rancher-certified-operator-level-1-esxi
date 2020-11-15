0ad12538638005ed5c22b760aabcb2ca3d8d9d0d
Hugo Gayosso <tribrhy@gmail.com>
2020-11-14 19:01:22 -0500

    Determine info for local webserver

--- infra.sh
+++ infra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 COMMAND=$1
+CONFIG_FILE=project/vars.yaml
 
 docker build . \
                 --build-arg MYUSER=$(id -n -u) \
@@ -23,15 +24,37 @@ function infra_container {
                         -e SSH_AUTH_SOCK="${SSH_AUTH_SOCK}" \
                         --env-file .env \
                         --workdir /files \
+                        -p 5000:5000 \
                         ${EXTRA_OPTIONS} \
                         infrabuilder ${COMMAND}
 }
 
+function get_webserver_info {
+        echo These are the possible IP addresses:
+        hostname -I
+        POSSIBLE_IP=$(hostname -I | cut -d' ' -f1)
+        read -p "Enter IP address of this machine: [${POSSIBLE_IP}]: " SELECTED_IP
+        SELECTED_IP=${SELECTED_IP:-${POSSIBLE_IP}}
+
+        read -p "Enter webserver port: [5000]: " WEB_PORT
+        WEB_PORT=${WEB_PORT:-5000}
+
+        echo "webserver_ip: ${SELECTED_IP}" >> ${CONFIG_FILE}
+        echo "webserver_port: ${WEB_PORT}"  >> ${CONFIG_FILE}
+
+}
+
+function delete_webserver_info {
+        sed -i '/^webserver/d' ${CONFIG_FILE}
+}
+
 case "$COMMAND" in
-        create ) infra_container create
+        create ) get_webserver_info
+                infra_container create
                 ;;
 
         destroy ) infra_container destroy
+                delete_webserver_info
                 ;;
 
         shell ) infra_container shell
